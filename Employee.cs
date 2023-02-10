0fb35d2f01c7def3185d021384331731549dbdd0
Divu <d1403kr3@gmail.com>
2023-02-10 16:22:28 +0530

    Assignment 1

---
+++ Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    //As said in the Assignment, 
+    // 1. Create a class called Employee
+    
+    class Employee // Class Employee created
+    {       
+        // 1.1 It has three private property as mentioned
+        private int Id;
+        private string Name;
+        private string Departmentname;
+
+        //3. Here I am creating event in the Employee Class 
+        public delegate void HandlingEvent(string msg);
+        public event HandlingEvent EventCaller;
+
+        public virtual void OnEventCaller(string message)
+        {
+            EventCaller?.Invoke(message);
+        }
+
+        // 1.2 Constructor should accept all three properties
+        public Employee(int empId, string empName, string empDepartmentName)
+        {
+            Id = empId;
+            Name = empName;
+            Departmentname = empDepartmentName;
+        }
+        //1.3 It should have three methods that individually returns the above properties
+        public int GetId()
+        {
+            //It fires a message when any of the class method is called as mentioned in the Assignment
+            OnEventCaller("GetId() method called");
+            return Id;
+        }
+        public string GetName()
+        {
+            //Message for GetName method
+            OnEventCaller("GetName() method called");
+            return Name;
+        }
+
+        public string GetDepartmentName()
+        {
+            //Message for GetDepartmentName() method
+            OnEventCaller("GetDepartmentName() method called");
+            return Departmentname;
+        }
+
+        //4. This is how I have created 3 overloaded methods that allow updating any of the Employee property
+        public void update(int empId)
+        {
+            Id = empId;
+        }
+        public void update(string empName)
+        {
+            Name = empName;
+        }
+        public void update(string empName, string empDepartmentName)
+        {
+            Name= empName;
+            Departmentname= empDepartmentName;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    //2. Program that accepts details from user
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //2.1 Asking the user for ID, Name, DepartmentName
+                Console.Write("Enter the employee Id: ");
+                int empId = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter you Name : ");
+                string empName = Console.ReadLine();
+
+                Console.Write("Enter the Department Name : ");
+                string empDepartmentName = Console.ReadLine();
+
+                //2.2 After getting details from user, here I am creating object of the Employee class
+                Employee emp = new Employee(empId, empName, empDepartmentName);
+                emp.EventCaller += OnEventCaller;
+                Console.WriteLine();
+
+                //2.3 After creating an object, This is how to print all three properties one by one
+                Console.WriteLine("ID =  " + emp.GetId());
+                Console.WriteLine("Name = " + emp.GetName());
+                Console.WriteLine("Department Name = " + emp.GetDepartmentName());
+                //For blank new line I am using Console.WriteLine
+                Console.WriteLine();
+
+                Console.WriteLine("....Updating Employee Details....");
+
+                Console.WriteLine();
+
+                //Getting user input for updating Employee details
+                Console.Write("Enter new Employee ID : ");
+                int newEmpId = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter new Employee Name : ");
+                string newEmpName = Console.ReadLine();
+
+                Console.Write("Enter new Department Name : ");
+                string newEmpDepartmentName = Console.ReadLine();
+
+                //To update
+                emp.update(newEmpId);
+                emp.update(newEmpName);
+                emp.update(newEmpDepartmentName);
+
+                Console.WriteLine();
+
+                //Printing the updated Message
+                Console.WriteLine("Updated ID =  " + emp.GetId());
+                Console.WriteLine("Updated Name = " + emp.GetName());
+                Console.WriteLine("Updated Department Name = " + emp.GetDepartmentName());
+            }
+
+            //Exception for any error Like invalid input in employee ID
+            catch(Exception e)
+            {
+                Console.WriteLine("Invalid Input\n{0}", e.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+                
+        private static void OnEventCaller(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
